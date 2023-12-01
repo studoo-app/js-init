17788d69c5c1750ce3e0dabc3938c664ef80fc2c
JPechberty <j.pechberty@gmail.com>
2023-12-01 08:50:11 +0100

    Update  correction exervices Conditions

--- conditions/exos/js/exercice-2.js
+++ conditions/exos/js/exercice-2.js
@@ -3,17 +3,3 @@ console.log("Script condition-2 chargé !")
 //Décommentez les lignes ci-dessous
 const password = prompt('Entrez un mot de passe')
 
-// Ecrire votre code ici, afficher si le mot de passe est considéré comme fort ou non à l'aide de console.log('votre réponse')
-let message = ""
-console.log(password.substring(3,4))
-if(
-    password.length > 8 &&
-    (password.includes("@") || password.includes("_")) &&
-    password.substring(3,4) === "T"
-){
-    message = "Mot de passe Fort !"
-}else{
-    message = "Mot de passe faible !"
-}
-
-console.log(message)
